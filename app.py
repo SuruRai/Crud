aa63c32e530dddeba737ae82d1df2d980951e729
SURUCHI RAI <87699252+SuruRai@users.noreply.github.com>
2022-11-25 11:59:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app.secret_key = 'many random bytes'
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_PASSWORD'] = '123456789'
 app.config['MYSQL_DB'] = 'crudapplication'
 
 mysql = MySQL(app)
